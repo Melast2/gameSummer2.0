efc83cf7f87025ff10832bf8fedc3ad3b331ea86
Melast2 <86879196+Melast2@users.noreply.github.com>
2021-07-12 01:44:23 +0300

    Qwerty
    qwerty

---
+++ app/src/main/java/com/example/studentslife/ItemActiveName.kt
@@ -0,0 +1,16 @@
+package com.example.studentslife
+
+object ItemActiveName {
+    var list: List<ItemActive>
+    init {
+        list = listOf<ItemActive>(
+            ItemActive("Телефон", 1, Keys.COLLEAGUE_ID,100),
+            ItemActive("Компьютер ", 2, Keys.DIPPER_ID,200),
+            ItemActive("Мак", 3, Keys.BOER_ID,300),
+            ItemActive("fbd", 4, Keys.GAME_ID, 400)
+             )
+    }
+    fun getInitialCounts() : List<Int> {
+        return listOf(100,200,300,400)
+    }
+}
